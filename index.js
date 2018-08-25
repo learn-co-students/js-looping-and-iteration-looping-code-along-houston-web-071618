85ef5e5cde48e3ed08ec83c409083ae59c4a758e
danrodz <daniel.a.rodriguez@dr.com>
2018-08-25 18:09:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function printBadges(names) {
+  for (const name in names) {
+    console.log(
+      `Welcome ${names[name]}! You are employee #${parseInt(name) + 1}.`
+    );
+  }
+  return names;
+}
+
+function tailsNeverFails() {
+  let tails = 0;
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+  return `You got ${tails} tails in a row!`;
+}
