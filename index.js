70d5bc3fc08bf1a82c2dde530daae8d4d370dc5a
stephen <stephen@farver.io>
2018-08-26 13:28:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function printBadges(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(`Welcome ${array[i]}! You are employee #${i+1}.`)
+  }
+  return array
+}
+
+function tailsNeverFails() {
+  let result = "tails"
+  let tailCount = -1
+
+  while (result == "tails") {
+    result = Math.random() < 0.5 ? "heads" : "tails"
+    tailCount++
+  }
+
+  return `You got ${tailCount} tails in a row!`
+}
